f53e31f8dda1e383ee3b47218e1b796006234bb8
m3ia <meianatividad@gmail.com>
2022-10-04 11:29:56 -0700

    Create new contact form

--- client/src/components/ContactsHome.jsx
+++ client/src/components/ContactsHome.jsx
@@ -1,18 +1,72 @@
 import {useEffect, useState} from "react";
+import NewContactForm from "./NewContactForm";
+
 const ContactsHome = () => {
   const [contacts, setContacts] = useState([]);
-  useEffect(() => {
-    const getContacts = async () => {
-      await fetch("http://localhost:8080/contacts")
-        .then((res) => res.json())
-        .then((res) => setContacts(res));
+  const [contact, setContact] = useState({
+    name: "",
+    number: "",
+    email: "",
+    notes: "",
+    photo: "",
+  });
+  const getContacts = async () => {
+    await fetch("http://localhost:8080/contacts")
+      .then((res) => res.json())
+      .then((res) => setContacts(res));
+  };
+
+  // Add Contact
+  const addNewContact = async (e) => {
+    e.preventDefault();
+
+    const newContact = {
+      name: contact.name,
+      number: contact.number,
+      email: contact.email,
+      notes: contact.notes,
+      photo: contact.photo,
     };
+
+    const res = await fetch("http://localhost:8080/newcontact", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      // Sent in with the request
+      body: JSON.stringify(newContact),
+    });
+
+    const content = await res.json();
+    console.log("content", content);
+    // setSightings([transformData(content), ...sightings]);
+    getContacts();
+
+    setContact({
+      name: "",
+      number: "",
+      email: "",
+      notes: "",
+      photo: "",
+    });
+  };
+
+  useEffect(() => {
     getContacts();
   }, []);
+
   return (
     <div className="container">
       <div className="title-bar">
-        <h2>Contacts</h2>
+        <h2>Contacts</h2>{" "}
+        <NewContactForm
+          contacts={contacts}
+          setContacts={setContacts}
+          contact={contact}
+          setContact={setContact}
+          addNewContact={addNewContact}
+        />
       </div>
       <div className="contacts-list">
         {contacts.map((contact, ind) => {
---
+++ client/src/components/NewContactForm.jsx
@@ -0,0 +1,172 @@
+import {useState, useEffect} from "react";
+import {Button, Modal, Form} from "react-bootstrap";
+
+function FormModal(props) {
+  // Get Individuals
+  // useEffect(() => {
+  //   const getIndividuals = async () => {
+  //     await fetch("http://localhost:8080/individuals")
+  //       .then((res) => res.json())
+  //       .then((res) => {
+  //         props.setIndividuals(() => [
+  //           ...res.map((individual) => {
+  //             return {
+  //               id: individual.id,
+  //               nickName: individual.nick_name,
+  //               seenDate: individual.seen_on,
+  //               speciesId: individual.species_id,
+  //             };
+  //           }),
+  //         ]);
+  //       });
+  //   };
+  //   getIndividuals();
+  // }, []);
+
+  return (
+    <Modal
+      show={props.show}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered>
+      <Modal.Header closeButton onClick={props.onHide}>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Add a New Contact
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>New Contact Form</h4>
+        <form id="add-contact" action="#">
+          <fieldset>
+            <p>
+              <label>Name</label>
+              <br />
+              <input
+                type="text"
+                id="add-name"
+                value={props.contact.name}
+                onChange={(e) => {
+                  props.setContact((prev) => ({
+                    ...prev,
+                    name: e.target.value,
+                  }));
+                }}
+              />
+            </p>
+            <p>
+              <label>Phone Number</label>
+              <br />
+              <input
+                type="text"
+                id="add-number"
+                value={props.contact.number}
+                onChange={(e) => {
+                  props.setContact((prev) => ({
+                    ...prev,
+                    number: e.target.value,
+                  }));
+                }}
+              />
+            </p>
+            <p>
+              <label>Email</label>
+              <br />
+              <input
+                type="text"
+                id="add-email"
+                value={props.contact.email}
+                onChange={(e) =>
+                  props.setContact((prev) => ({
+                    ...prev,
+                    email: e.target.value,
+                  }))
+                }
+              />
+            </p>
+            <p>
+              <label>Photo</label>
+              <br />
+              <input
+                type="text"
+                id="add-photo"
+                value={props.contact.photo}
+                placeholder="Add photo link"
+                onChange={(e) =>
+                  props.setContact((prev) => ({
+                    ...prev,
+                    photo: e.target.value,
+                  }))
+                }
+              />
+            </p>
+            <p>
+              <label>Notes</label>
+              <br />
+              <input
+                type="text"
+                id="add-notes"
+                value={props.contact.notes}
+                onChange={(e) =>
+                  props.setContact((prev) => ({
+                    ...prev,
+                    notes: e.target.value,
+                  }))
+                }
+              />
+            </p>
+          </fieldset>
+        </form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button type="submit" onClick={props.onHide}>
+          Close
+        </Button>
+        <Button
+          variant="primary"
+          type="submit"
+          onClick={(e) => {
+            props.addNewContact(e);
+            props.onHide();
+          }}>
+          {" "}
+          Submit
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+function AddSightingForm({
+  contacts,
+  setContacts,
+  contact,
+  setContact,
+  addNewContact,
+}) {
+  const [modalShow, setModalShow] = useState(false);
+  // const [individuals, setIndividuals] = useState([]);
+  return (
+    <>
+      <Button
+        variant="primary"
+        onClick={() => setModalShow(true)}
+        className="launch-modal-btn">
+        <span className="material-symbols-outlined">add</span>
+      </Button>
+
+      <FormModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        contacts={contacts}
+        setContacts={setContacts}
+        contact={contact}
+        setContact={setContact}
+        addNewContact={addNewContact}
+        // individuals={individuals}
+        // setIndividuals={setIndividuals}
+      />
+    </>
+  );
+}
+
+export default AddSightingForm;
