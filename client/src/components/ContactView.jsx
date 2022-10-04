7197ed6d6e875498b10d4e3d0e90ec8574b6d1f6
m3ia <meianatividad@gmail.com>
2022-10-04 14:41:05 -0700

    Center contact view card

--- client/src/components/ContactView.jsx
+++ client/src/components/ContactView.jsx
@@ -1,30 +1,53 @@
 const ContactView = ({contactView, setContactView}) => {
   return (
-    <div>
-      <p>contact view name: {contactView.name}</p>
-      {contactView.numbers.length && contactView.numbers.length >= 2 ? (
-        contactView.numbers.map((item, ind) => {
-          return (
-            <p key={ind}>
-              Phone Number {ind + 1}: {item}
-            </p>
-          );
-        })
-      ) : (
-        <p>Phone Number: {contactView.number}</p>
-      )}
-      <p>contact view email: {contactView.email}</p>
-      <p>contact view notes: {contactView.notes}</p>
-      <p>
-        contact view photo: <img src={contactView.photo} alt="profile" />
-      </p>
-      <div>
-        <button
+    <div className="contact-view-container">
+      <div className="contact-view-menu">
+        <span
+          className="material-symbols-outlined"
           onClick={() => {
             setContactView({});
           }}>
-          Back
-        </button>
+          arrow_back_ios
+        </span>
+      </div>
+      <div className="contact-view-card">
+        <div className="contact-view-photo">
+          <p>
+            <img
+              src={contactView.photo}
+              alt="profile"
+              style={{
+                borderRadius: "50%",
+                height: 150,
+                width: 150,
+              }}
+            />
+          </p>
+        </div>
+        <div className="contact-view-body">
+          <h3>{contactView.name}</h3>
+          {contactView.numbers.length && contactView.numbers.length >= 2 ? (
+            contactView.numbers.map((item, ind) => {
+              return (
+                <p key={ind}>
+                  <strong>Phone Number {ind + 1}:</strong> {item}
+                </p>
+              );
+            })
+          ) : (
+            <p>
+              <strong>Phone Number:</strong> {contactView.number}
+            </p>
+          )}
+          <p>
+            <strong>Email:</strong> {contactView.email}
+          </p>
+          <p>
+            <strong>Notes:</strong>{" "}
+            {contactView.notes ? contactView.notes : "none"}
+          </p>
+        </div>
+        <div className="contact-view-footer"></div>
       </div>
     </div>
   );
