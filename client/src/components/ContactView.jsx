9972b7a081db2e00f1ec597d9a699ccf41f508ef
m3ia <meianatividad@gmail.com>
2022-10-04 14:02:03 -0700

    add basic single contact view

--- client/src/components/ContactView.jsx
+++ client/src/components/ContactView.jsx
@@ -2,9 +2,22 @@ const ContactView = ({contactView, setContactView}) => {
   return (
     <div>
       <p>contact view name: {contactView.name}</p>
-      <p>contact view number: {contactView.number}</p>
-      <p>contact view name: {contactView.name}</p>
-      <p>contact view name: {contactView.name}</p>
+      {contactView.numbers.length && contactView.numbers.length >= 2 ? (
+        contactView.numbers.map((item, ind) => {
+          return (
+            <p key={ind}>
+              Phone Number {ind + 1}: {item}
+            </p>
+          );
+        })
+      ) : (
+        <p>Phone Number: {contactView.number}</p>
+      )}
+      <p>contact view email: {contactView.email}</p>
+      <p>contact view notes: {contactView.notes}</p>
+      <p>
+        contact view photo: <img src={contactView.photo} alt="profile" />
+      </p>
       <div>
         <button
           onClick={() => {
--- client/src/components/ContactsHome.jsx
+++ client/src/components/ContactsHome.jsx
@@ -3,8 +3,9 @@ import NewContactForm from "./NewContactForm";
 import ContactView from "./ContactView";
 
 const ContactsHome = () => {
+  // contacts holds all contacts in an array of objects
   const [contacts, setContacts] = useState([]);
-  const [contactIds, setContactIds] = useState([]);
+  // contact holds information for a new potential contact
   const [contact, setContact] = useState({
     name: "",
     number: "",
@@ -12,13 +13,14 @@ const ContactsHome = () => {
     notes: "",
     photo: "",
   });
+  // contactView contains info for ONE contact that the user wants to view
   const [contactView, setContactView] = useState({});
 
   const getContacts = async () => {
     await fetch("http://localhost:8080/contacts")
       .then((res) => res.json())
       .then((res) => {
-        setContacts(res);
+        // Store data in new list with objects that hold 2+ numbers
         let contactsArr = [];
         let ids = [];
         for (let item of res) {
@@ -27,7 +29,7 @@ const ContactsHome = () => {
             let contactObj = {
               id: item.id,
               name: item.name,
-              number: [item.number],
+              numbers: [item.number],
               email: item.email,
               photo: item.photo,
               notes: item.notes,
@@ -35,10 +37,10 @@ const ContactsHome = () => {
             contactsArr.push(contactObj);
           } else {
             let itemIndex = ids.indexOf(item.id);
-            contactsArr[itemIndex].number.push(item.number);
+            contactsArr[itemIndex].numbers.push(item.number);
           }
         }
-        setContactIds(contactsArr);
+        setContacts(contactsArr);
       });
   };
 
