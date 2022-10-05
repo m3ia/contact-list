ceb27d998fbabf934f153511d977fd39dfeed566
m3ia <meianatividad@gmail.com>
2022-10-05 13:18:21 -0700

    Add toggles for edit mode

--- client/src/components/ContactView.jsx
+++ client/src/components/ContactView.jsx
@@ -65,33 +65,116 @@ const ContactView = ({contactView, setContactView}) => {
           </h3>
           {contactView.numbers.length && contactView.numbers.length >= 2 ? (
             contactView.numbers.map((item, ind) => {
-              return (
+              return editMode ? (
+                <p key={item.numberId}>
+                  <label>Number {ind + 1}:</label>{" "}
+                  <input
+                    type="text"
+                    defaultValue={item.number}
+                    onChange={(e) => {
+                      setContactUpdate((prev) => ({
+                        ...prev,
+                        numbers: [
+                          {
+                            numberId: item.numberId,
+                            number: e.target.value,
+                          },
+                        ],
+                      }));
+                    }}
+                  />
+                </p>
+              ) : (
                 <p key={item.numberId}>
                   <strong>Phone Number {ind + 1}:</strong> {item.number}
                 </p>
               );
             })
+          ) : editMode ? (
+            <p>
+              <label>Name:</label>{" "}
+              <input
+                type="text"
+                defaultValue={contactView.numbers[0].number}
+                onChange={(e) => {
+                  setContactUpdate((prev) => ({
+                    ...prev,
+                    numbers: [
+                      {
+                        numberId: contactView.numbers[0].numberId,
+                        number: e.target.value,
+                      },
+                    ],
+                  }));
+                }}
+              />
+            </p>
           ) : (
             <p>
               <strong>Phone Number:</strong> {contactView.numbers[0].number}
             </p>
           )}
-          <p>
-            <strong>Email:</strong> {contactView.email}
-          </p>
-          <p>
-            <strong>Notes:</strong>{" "}
-            {contactView.notes ? contactView.notes : "none"}
-          </p>
+
+          {editMode ? (
+            <p>
+              <label>Email:</label>{" "}
+              <input
+                type="text"
+                defaultValue={contactView.email}
+                onChange={(e) => {
+                  setContactUpdate((prev) => ({
+                    ...prev,
+                    email: e.target.value,
+                  }));
+                }}
+              />
+            </p>
+          ) : (
+            <p>
+              <strong>Email:</strong> {contactView.email}
+            </p>
+          )}
+
+          {editMode ? (
+            <p>
+              <label>Notes:</label>{" "}
+              <input
+                type="text"
+                defaultValue={contactView.notes}
+                onChange={(e) => {
+                  setContactUpdate((prev) => ({
+                    ...prev,
+                    notes: e.target.value,
+                  }));
+                }}
+              />
+            </p>
+          ) : (
+            <p>
+              <strong>Notes:</strong>{" "}
+              {contactView.notes ? contactView.notes : "none"}
+            </p>
+          )}
         </div>
         <div className="contact-view-footer">
-          <span
-            className="material-symbols-outlined menu-icons"
-            onClick={() =>
-              setEditMode((curr) => (curr === true ? false : true))
-            }>
-            edit_square
-          </span>
+          {editMode ? (
+            <p>
+              <span
+                className="material-symbols-outlined menu-icons"
+                onClick={() => setEditMode(false)}>
+                cancel
+              </span>
+              <span className="material-symbols-outlined menu-icons">save</span>
+            </p>
+          ) : (
+            <p>
+              <span
+                className="material-symbols-outlined menu-icons"
+                onClick={() => setEditMode(true)}>
+                edit_square
+              </span>
+            </p>
+          )}
         </div>
       </div>
     </div>
